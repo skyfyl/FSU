b29268667e0352726395af91b4c7f48d2ccd1344
Kobe Peng <pengchuanhui2010@gmail.com>
2024-02-25 14:12:15 +0800

    修正

--- FSU.user.js
+++ FSU.user.js
@@ -3028,7 +3028,7 @@ if(a.hasOwnProperty("_fsuLeag") && e.item.leagueId > 0){
         }
 
         // return resultList.length === 0 ? resultList : _.cloneDeep(resultList.filter(i => { return i.rating >= originPlayer.rating}));
-        return resultList.length === 0 ? resultList : _.cloneDeep(resultList.filter(i => { return i.rating <= originPlayer.rating}).sort((a, b) => a.rating - b.rating));
+        return resultList.length === 0 ? resultList : _.cloneDeep(resultList.sort((a, b) => a.rating - b.rating));
     }
     //默契球员读取程序
     events.SBCSetChemPlayers = async(e) => {
